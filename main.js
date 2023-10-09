dd7e3fa1ce0f8b9d6a8c228b93fe784093036690
Edward Tanguay (windows) <edward@tanguay.info>
2023-10-09 10:14:25 +0200

    002

--- main.js
+++ main.js
@@ -6,8 +6,17 @@ const ex001 = () => {
 	return items.filter(m => /\d\d\d/g.test(m));
 }
 
+// get an array of all numbers from strings
+const ex002 = () => {
+	const items = ['askjd732sd', 'skdfjs', 'asddf672', 'sdfsdf', 'skdf3828ks111d', '738sdfsdf'];
+	const regex = /\d+/g;
+	return items.filter(m => m.match(regex)).map(m => m.match(regex));
+}
+
 document.querySelector('#app').innerHTML = /*html*/ `
 <h1>Regex Learn Site</h1>
 <h2>ex001</h2>
 ${ex001().join(', ')}
+<h2>ex002</h2>
+${ex002().join(', ')}
 `;
